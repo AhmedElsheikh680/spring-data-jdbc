ed618951db40d2e2df4b24826e0a93c9308928cc
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2023-02-08 06:20:11 +0200

    Adding OjectMapper

--- src/main/java/com/spring/SpringDataJdbcApplication.java
+++ src/main/java/com/spring/SpringDataJdbcApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDataJdbcApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringDataJdbcApplication.class, args);
     }
 
--- src/main/java/com/spring/controller/EmployeeController.java
+++ src/main/java/com/spring/controller/EmployeeController.java
@@ -5,11 +5,13 @@ import com.spring.model.Employee;
 import com.spring.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("api/v1/employee")
 public class EmployeeController {
 
@@ -17,16 +19,19 @@ public class EmployeeController {
     private EmployeeRepo employeeRepo;
 
     @GetMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<?> findEmpById(@PathVariable Long id){
         return ResponseEntity.ok(employeeRepo.findById(id));
     }
 
     @GetMapping("/filter/{name}")
+    @ResponseBody
     public ResponseEntity<List<Employee>> findEmpByName(@PathVariable String name){
         return ResponseEntity.ok(employeeRepo.findByName(name));
     }
 
     @GetMapping("/filter/{name}/{salary}")
+    @ResponseBody
     public ResponseEntity<List<Employee>> findEmpByNameAndSalary(@PathVariable String name, @PathVariable String salary){
         return ResponseEntity.ok(employeeRepo.findByNameAndSalary(name, salary));
     }
@@ -42,21 +47,25 @@ public class EmployeeController {
 //    }
 
     @GetMapping("")
+    @ResponseBody
     public ResponseEntity<Iterable<Employee>> findAllEmps(){
         return ResponseEntity.ok(employeeRepo.findAll());
     }
 
     @PostMapping("")
+    @ResponseBody
     public ResponseEntity<?> addEmp(@RequestBody Employee employee){
         return ResponseEntity.ok(employeeRepo.save(employee));
     }
 
     @PutMapping("")
+    @ResponseBody
     public ResponseEntity<?> updateEmp(@RequestBody Employee employee){
         return ResponseEntity.ok(employeeRepo.save(employee));
     }
 
     @DeleteMapping("/{id}")
+    @ResponseBody
     public void deleteEmp(@PathVariable Long id){
          employeeRepo.deleteById(id);
     }
